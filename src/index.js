64d57538f7ad62ae243c13e6b847599cc50964f2
Eric Laursen <ericjlaursen@gmail.com>
2020-02-01 11:05:15 -0500

    more mock content, some js stuff

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
+const dummyFn = () => 'DummyFn String';
+let mockObj = {
+	a: 'aString'
+}
+let otherObj = {b : 'bString'}
 console.log('This is the beginning of a frontend build system');
+console.log(dummyFn());
+console.log(JSON.stringify({...mockObj, ...otherObj}));
